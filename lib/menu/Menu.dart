c86f1ee8b3d8cddf69bcf4074a81f53ccf1cb0ac
Daroberho <david.hohloch@t-online.de>
2021-12-20 11:42:45 +0100

    Added the Menu with no functionality

---
+++ lib/menu/Menu.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:app/menu/widgets/menu_buttons.dart';
+import 'package:app/mode/Mode.dart';
+import 'package:app/game/Game.dart';
+import 'package:app/settings/Settings.dart';
+
+class GameMenu extends StatelessWidget {
+  const GameMenu({Key? key}) : super(key: key);
+
+  final Map<String, Widget> _screens = const {
+    "Start": Game(),
+    "Mode": Mode(),
+    "Settings": Settings(),
+  };
+
+  List<Widget> _buildButtons() {
+    return _screens.entries.map((e) => MenuButton(name: e.key, page: e.value)).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Interactive Labyrinth"),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.white, Colors.blueGrey.shade200],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: _buildButtons(),
+          ),
+        ),
+      ),
+    );
+  }
+}
